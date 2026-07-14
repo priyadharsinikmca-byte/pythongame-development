fa9b5f8103c62460d7584799651729454b861da9
Rokith <priyadharsinikmca@googlemail.com>
2026-07-14 18:00:19 +0100

    Add List.py with 1D and 2D list examples
    Initialize List.py with examples of 1D and 2D lists, including printing elements and their lengths.

---
+++ List.py
@@ -0,0 +1,19 @@
+list1=[20,21,24,25,26,28,29,36]
+print(list1)
+
+for i in list1:
+    print(i)
+
+l2 =[[1,2,3],[4,5,6],[7,8,9]]
+print(l2)
+print(len(l2))
+
+print(l2[0][0])
+
+print(l2[2][2])
+#looping through while using 2d list
+
+for i in range (0,len(l2)):
+    for j in range(0,len(l2[i])):
+        print(l2[i][j])
+#take a input from a list and print the elements
